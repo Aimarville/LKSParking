f3066f045c83bb30608e29fdfa4e0a7c5e3b95f0
Aimarville <avillegas016@ikasle.ehu.eus>
2026-05-08 18:11:15 +0200

    Configurar SonarCloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,13 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.sonar)
+}
+
+sonar {
+    properties {
+        property("sonar.projektKey", "Aimarville_LKSParking")
+        property("sonar.organization", "aimarville")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
