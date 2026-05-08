976c4c6e69934b93a67be76c9f19ccd793b8ea81
Aimarville <avillegas016@ikasle.ehu.eus>
2026-05-08 18:20:24 +0200

    Configurar SonarCloud 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,10 @@ sonar {
         property("sonar.projektKey", "Aimarville_LKSParking")
         property("sonar.organization", "aimarville")
         property("sonar.host.url", "https://sonarcloud.io")
+
+        property("sonar.sources", "app/src/main/java,app/src/main/kotlin")
+        property("sonar.binaries", "app/build/intermediates/javac/debug/classes,app/build/tmp/kotlin-classes/debug")
+        property("sonar.java.binaries", "app/build/intermediates/javac/debug/classes")
+        property("sonar.junit.reportPaths", "app/build/test-results/testDebugUnitTest")
     }
 }
